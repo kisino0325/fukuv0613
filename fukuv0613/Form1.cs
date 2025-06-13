070ef53f30dc280751bee74a2eb0a1064345ede9
kishino <kisino0325@gmail.com>
2025-06-13 13:50:54 +0900

    fukuv0613

--- fukuv0613/Form1.cs
+++ fukuv0613/Form1.cs
@@ -2,9 +2,41 @@ namespace fukuv0613
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var mpos = MousePosition;
+            var fpos = PointToClient(mpos);
+            label1.Left += vx;
+            label1.Top += vy;
+            if (label1.Left < 0)
+            {
+                vx = Math.Abs(vx);
+            }
+            else if (label1.Right > ClientSize.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+            if (label1.Top < 0)
+            {
+                vy = Math.Abs(vy);
+            }
+            else if (label1.Bottom > ClientSize.Height)
+            {
+                vy = -Math.Abs(vy);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
