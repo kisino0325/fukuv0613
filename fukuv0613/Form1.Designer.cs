325536b04b26d4c0d8b2275ee45d6fe544011bc9
kishino <kisino0325@gmail.com>
2025-11-20 12:03:19 +0900

    Update Form1.Designer.cs

--- fukuv0613/Form1.Designer.cs
+++ fukuv0613/Form1.Designer.cs
@@ -41,6 +41,7 @@
             label1.Size = new Size(38, 15);
             label1.TabIndex = 0;
             label1.Text = "label1";
+            label1.UseWaitCursor = true;
             label1.Click += label1_Click;
             // 
             // timer1
